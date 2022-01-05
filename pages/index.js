80d701bfb00c7e8dd68e1570fef58a5652af7b9f
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-01-05 11:33:33 -0500

    Updating package.json

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Nav from "../components/Nav";
 import axios from "axios";
 import Flag from "react-world-flags";
